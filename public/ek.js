1971b3e04148f084f5a9a6cc5a8c72a28e2172d0
amateur5 <122245513+amateur5@users.noreply.github.com>
2025-07-31 03:34:32 +0200

    Update ek.js

--- public/ek.js
+++ public/ek.js
@@ -1,81 +1,79 @@
-// Stilizacija diva (dodao padding i box-sizing za resize sa svih strana)
-const waveformDiv = document.getElementById('waveform');
-Object.assign(waveformDiv.style, {
-  width: '400px',
-  height: '100px',
-  left: 10px,
-  top : 200px,
-  border: '2px dashed #555',
-  resize: 'both',
-  overflow: 'auto',
-  cursor: 'move',
-  background: 'repeating-linear-gradient(45deg, #333, #333 10px, #444 10px, #444 20px)',
-  boxSizing: 'border-box',
-  padding: '10px',
-  position: 'relative' // za pozicioniranje talasa
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const waveformDiv = document.getElementById('waveform');
 
-document.body.style.background = '#000';
+  // Stilovi diva
+  Object.assign(waveformDiv.style, {
+    width: '400px',
+    height: '100px',
+    border: '2px dashed #555',
+    resize: 'both',
+    overflow: 'auto',
+    cursor: 'move',
+    background: 'repeating-linear-gradient(45deg, #333, #333 10px, #444 10px, #444 20px)',
+    boxSizing: 'border-box',
+    padding: '10px',
+    position: 'absolute',
+    left: '10px',
+    top: '200px'
+  });
 
-// Drag funkcija (micanje diva)
-waveformDiv.onmousedown = function (event) {
-  let shiftX = event.clientX - waveformDiv.getBoundingClientRect().left;
-  let shiftY = event.clientY - waveformDiv.getBoundingClientRect().top;
+  // Drag funkcija
+  waveformDiv.onmousedown = function (event) {
+    let shiftX = event.clientX - waveformDiv.getBoundingClientRect().left;
+    let shiftY = event.clientY - waveformDiv.getBoundingClientRect().top;
 
-  function moveAt(pageX, pageY) {
-    waveformDiv.style.position = 'absolute';
-    waveformDiv.style.left = pageX - shiftX + 'px';
-    waveformDiv.style.top = pageY - shiftY + 'px';
-  }
+    function moveAt(pageX, pageY) {
+      waveformDiv.style.left = pageX - shiftX + 'px';
+      waveformDiv.style.top = pageY - shiftY + 'px';
+    }
 
-  function onMouseMove(event) {
-    moveAt(event.pageX, event.pageY);
-  }
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
 
-  document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mousemove', onMouseMove);
 
-  waveformDiv.onmouseup = function () {
-    document.removeEventListener('mousemove', onMouseMove);
-    waveformDiv.onmouseup = null;
+    waveformDiv.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      waveformDiv.onmouseup = null;
+    };
   };
-};
 
-waveformDiv.ondragstart = function () {
-  return false;
-};
+  waveformDiv.ondragstart = () => false;
 
-// Kreiramo unutrašnji container za Wavesurfer da ne prekrije padding
-const innerWaveform = document.createElement('div');
-innerWaveform.style.width = '100%';
-innerWaveform.style.height = '100%';
-waveformDiv.appendChild(innerWaveform);
+  // Kreiramo unutrašnji container za Wavesurfer
+  const innerWaveform = document.createElement('div');
+  innerWaveform.style.width = '100%';
+  innerWaveform.style.height = '100%';
+  waveformDiv.appendChild(innerWaveform);
 
-// Wavesurfer setup sa containerom unutar diva
-const wavesurfer = WaveSurfer.create({
-  container: innerWaveform,
-  waveColor: 'yellow',
-  progressColor: 'blue',
-  height: 80,
-  barWidth: 2,
-  responsive: true,
-  interact: false
-});
+  // Wavesurfer setup
+  const wavesurfer = WaveSurfer.create({
+    container: innerWaveform,
+    waveColor: 'yellow',
+    progressColor: 'blue',
+    height: 80,
+    barWidth: 2,
+    responsive: true,
+    interact: false
+  });
 
-wavesurfer.load('https://stream.zeno.fm/krdfduyswxhtv');
+  wavesurfer.load('https://stream.zeno.fm/krdfduyswxhtv');
 
-// Promena boje po jačini zvuka (na osnovu analize frekvencija)
-wavesurfer.on('audioprocess', () => {
-  const analyser = wavesurfer.backend.getAnalyserNode();
-  const dataArray = new Uint8Array(analyser.frequencyBinCount);
-  analyser.getByteFrequencyData(dataArray);
+  // Promena boje po jačini zvuka (na osnovu analize frekvencija)
+  wavesurfer.on('audioprocess', () => {
+    const analyser = wavesurfer.backend.getAnalyserNode();
+    const dataArray = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(dataArray);
 
-  let avg = dataArray.reduce((a, b) => a + b) / dataArray.length;
+    let avg = dataArray.reduce((a, b) => a + b) / dataArray.length;
 
-  if (avg < 30) {
-    wavesurfer.setWaveColor('yellow');
-  } else if (avg < 100) {
-    wavesurfer.setWaveColor('blue');
-  } else {
-    wavesurfer.setWaveColor('red');
-  }
+    if (avg < 30) {
+      wavesurfer.setWaveColor('yellow');
+    } else if (avg < 100) {
+      wavesurfer.setWaveColor('blue');
+    } else {
+      wavesurfer.setWaveColor('red');
+    }
+  });
 });
